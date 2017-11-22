4dd2ae6b0a0b1d0c157d22643bc324749663b548
jksison <johnk.sison@gmail.com>
2017-11-21 22:10:43 -0500

    jmeter exercise

---
+++ JavaMemoryManagement/StudentManager/bin/com/virtualpairprogrammers/domain/Address_.java
@@ -0,0 +1,16 @@
+package com.virtualpairprogrammers.domain;
+
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value = "org.hibernate.jpamodelgen.JPAMetaModelEntityProcessor")
+@StaticMetamodel(Address.class)
+public abstract class Address_ {
+
+	public static volatile SingularAttribute<Address, String> city;
+	public static volatile SingularAttribute<Address, String> street;
+	public static volatile SingularAttribute<Address, String> zipOrPostcode;
+
+}
+
---
+++ JavaMemoryManagement/tomcat/webapps/mywebapp/WEB-INF/classes/com/virtualpairprogrammers/domain/Tutor_.java
@@ -0,0 +1,21 @@
+package com.virtualpairprogrammers.domain;
+
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SetAttribute;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value = "org.hibernate.jpamodelgen.JPAMetaModelEntityProcessor")
+@StaticMetamodel(Tutor.class)
+public abstract class Tutor_ {
+
+	public static volatile SetAttribute<Tutor, Student> supervisionGroup;
+	public static volatile SetAttribute<Tutor, Subject> subjectsQualifiedToTeach;
+	public static volatile SingularAttribute<Tutor, String> name;
+	public static volatile SingularAttribute<Tutor, Integer> id;
+	public static volatile SingularAttribute<Tutor, Integer> salary;
+	public static volatile SingularAttribute<Tutor, Integer> version;
+	public static volatile SingularAttribute<Tutor, String> staffId;
+
+}
+
