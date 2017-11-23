8450e465359cb772f8e3aab1a8ffce524e435177
jksison <johnk.sison@gmail.com>
2017-11-22 22:26:27 -0500

    object oriented design initial commit

---
+++ ObjectOrientedDesign/Prerequisite_OOP/src/Earth.java
@@ -0,0 +1,17 @@
+
+public class Earth {
+
+	public static void main(String[] args) {
+
+		Human tom;
+		
+		tom = new Human();
+		tom.name = "Tom";
+		tom.eyeColor = "Brown";
+		tom.heightInInches = 72;
+		tom.age = 5;
+		
+		tom.speak();
+	}
+
+}
