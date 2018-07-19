c344f2f556122ee6585a8bb089a37c509c8065e5
jksison <johnk.sison@gmail.com>
2018-07-18 22:21:35 -0400

    test TreeSet ds

---
+++ java-util-collections/TreeSetTest/src/TestCollection11.java
@@ -0,0 +1,21 @@
+import java.util.Iterator;
+import java.util.TreeSet;
+
+public class TestCollection11 {
+
+	public static void main(String[] args) {
+		// Creating and adding elements
+		TreeSet<String> al = new TreeSet<String>();
+		al.add("Ravi");
+		al.add("Vijay");
+		al.add("Ravi");
+		al.add("Ajay");
+		
+		// Traversing elements
+		Iterator<String> itr = al.iterator();
+		while (itr.hasNext()) {
+			System.out.println(itr.next());
+		}
+	}
+
+}
